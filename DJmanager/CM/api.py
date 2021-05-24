b7db50f3a8a2959d18ceb83bdd00b355be0ef6d5
Ali <azap413@yahoo.com>
2021-05-24 11:41:17 +0500

    Added checks for POST request

--- DJmanager/CM/api.py
+++ DJmanager/CM/api.py
@@ -35,9 +35,20 @@ class RideViewSet(viewsets.ModelViewSet):
             dest = request.data.get('destination')
             drivers = DriverSerializer(
                 Driver.objects.filter(destination=dest), many=True)
-            super().create(request, *args, **kwargs)
-            return Response(drivers.data)
+            drivercount = Driver.objects.filter(destination=dest).count()
+            if drivercount > 0:
+                super().create(request, *args, **kwargs)
+                return Response(drivers.data)
+            else:
+                return Response({"status": "No drivers Found"}, 200)
         return Response({"Status": "Error"}, 200)
+
+    def update(self, request, *args, **kwargs):
+        ridestatus = request.data.get('status')
+        if ridestatus == 'passengerconfirmed':
+            super.update(request, *args, **kwargs)
+            return Response({"status": "confirming driver"})
+        return Response({"status": "passenger not confirmed"})
 
 
 class SponsorViewSet(viewsets.ModelViewSet):
